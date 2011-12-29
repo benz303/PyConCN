8ba6f492a276a1fe458f528f517d4111af08ee89
ben <ben@zfben.(none)>
2011-12-29 11:30:50 +0800

    pre release

--- apps/2012/views.py
+++ apps/2012/views.py
@@ -8,28 +8,35 @@ from uliweb import request
 
 @expose('/2012/')
 def index():
+    settings.LAYOUT.SUBTITLE = '首页'
     return {} #'<h1>Hello, Uliweb</h1>'
 
 @expose('/2012')
 class siteView(object):
         
     def about(self):
-        
+        settings.LAYOUT.SUBTITLE = '关于大会'
         return dict(page=dict(pagename='about',cndata='')) 
     
     def schedule(self):
+        settings.LAYOUT.SUBTITLE = '日程安排'
         return dict(page=dict(pagename='schedule',cndata=''))
     
     def collections(self):
+        settings.LAYOUT.SUBTITLE = '大会集锦'
         return dict(page=dict(pagename='collections',cndata=''))    
 
     def registration(self):
+        settings.LAYOUT.SUBTITLE = '报名注册'
         return dict(page=dict(pagename='registration',cndata=''))
         
     def sponsors(self):
+        settings.LAYOUT.SUBTITLE = '支持单位'
         return dict(page=dict(pagename='sponsors',cndata=''))
 
     def volunteer(self):
+        settings.LAYOUT.SUBTITLE = '志愿者'
         return dict(page=dict(pagename='volunteer',cndata=''))
     def venue(self):
+        settings.LAYOUT.SUBTITLE = '会场交通'
         return dict(page=dict(pagename='venue',cndata=''))    
